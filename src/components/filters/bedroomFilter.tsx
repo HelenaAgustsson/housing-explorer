fd9fa5af92042dbd11784e5af1dcf9122055cc45
Helena Agustsson <agustssonhelena@gmail.com>
2026-05-19 15:32:06 +0100

    add lucide react icon

--- src/components/filters/bedroomFilter.tsx
+++ src/components/filters/bedroomFilter.tsx
@@ -1,11 +1,25 @@
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from "../ui/dropdown-menu";
-import { Button } from "../ui/button";
+"use client";
+
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 
 export function BedroomFilter() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline">Number of bedrooms</Button>
+                <Button variant="outline">
+                    Number of bedrooms
+                    <ChevronDown className="h-4 w-4" />
+                </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuGroup>
